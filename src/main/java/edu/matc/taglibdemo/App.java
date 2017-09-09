9ccb28e0dd36523b95d3c11bb5f7508c502d5684
Jamie Kruser <jmkruser@madisoncollege.edu>
2017-09-09 00:10:06 -0500

    Starting Week 3 Activity

---
+++ src/main/java/edu/matc/taglibdemo/App.java
@@ -0,0 +1,13 @@
+package edu.matc.taglibdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
